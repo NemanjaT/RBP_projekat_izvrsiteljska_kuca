933ca1b8f1d01807a11c26917b59979507a6dff9
Nemanja Tozic <nemanjat94@hotmail.com>
2016-06-02 22:12:21 +0200

    dodao initial data

--- backend/initial_data.sql
+++ backend/initial_data.sql
@@ -1,115 +1,124 @@
 -- ### initial_data.sql ###
 
-use [VRTIC]
+use [IZVRSITELJSKA_KUCA]
 go
 begin
-	truncate table TAB_STARATELJI_DECA;
-	truncate table TAB_DECA;
-	truncate table TAB_GRUPE;
-	truncate table TAB_STARATELJI;
-	truncate table TAB_FINANSIJSKE_GRUPE;
-	truncate table TAB_INCIDENTI;
-	truncate table TAB_OSTAVLJANJA_DECE;
-	truncate table TAB_PREUZIMANJA_DECE;
-	truncate table TAB_RACUNI;
-	truncate table TAB_UPLATE;
+	truncate table T_TRANSAKCIJE;
+	truncate table T_DUGOVANJA;
+	truncate table T_IZVR_KUCA_GRUPE;
+	truncate table T_IZVRSITELJSKE_KUCE;
+	truncate table T_ORG_JED_GRUPE;
+	truncate table T_ORGANIZACIONE_JEDINICE;
+	truncate table T_GRUPE_ORG_JEDINICA;
+	truncate table T_IZVRSITELJI;
+	truncate table T_TIPOVI_IZVRSITELJA;
+	truncate table T_LICA;
 end
 go
 begin
-	set identity_insert TAB_GRUPE on;
-	insert into TAB_GRUPE (ID, IME_GRUPE, SPRAT, VASPITACI, CENA)
-	values (1000, 'Cvetici', 2, 'Dragana Mitic, Snezana Savic', '3000.0'),
-		   (1001, 'Autici', 1, 'Jagorka Stamonasivljevic Jovanovic, John Doe, Foo Bar', '3200.0');
-	set identity_insert TAB_GRUPE off;
+	set identity_insert T_LICA on;
+	insert into T_LICA (ID, IME, PREZIME, DATUM_RODJENJA, JMBG, ADRESA_STANOVANJA, DODATNE_INFORMACIJE)
+	values (1000, 'Nemanja', 'Tozic', cast('09/28/1994' as date), '123456789123', 'Stevana Lukovica 16', ''),
+		   (1001, 'Pera', 'Peric', cast('01/02/1989' as date), '234567891234', 'Neki Put 16a', ''),
+		   (1002, 'Deni', 'Devito', cast('11/17/1944' as date), '345678912345', 'Nju Dzerzijeva 17b', ''),
+		   (1003, 'Dragica', 'Stankovic', cast('04/05/1973' as date), '46789123456', 'Tamo Negde 177', 'dodatna informacija'),
+		   (1004, 'Mila', 'Stankovic', cast('07/06/1956' as date), '567891234567', 'Jos Jedna Adresa 18', 'druga  informacija');
+	set identity_insert T_LICA off;
 
-	set identity_insert TAB_DECA on;
-	insert into TAB_DECA (ID, IME, PREZIME, DATUM_RODJENJA, GRUPA_ID, DODATNE_INFORMACIJE)
-	values (1000, 'Kika', 'Simic', CAST('01/01/2010' as date), 1000, ''),
-		   (1001, 'Milja', 'Trifunovic', CAST('12/31/2009' as date), 1000, 'Cerka Sinise Trifunovica'),
-		   (1002, 'Jelena', 'Andjelkovic', CAST('05/08/2008' as date), 1000, 'Cerka Milosa Andjelkovica'),
-		   (1003, 'Mina', 'Tozic', CAST('12/06/2011' as date), 1000, 'Cerka Nemanje Tozica'),
-		   (1004, 'Brena', 'Lepa', CAST('05/04/2010' as date), 1000, ''),
-		   (1005, 'Svetlana', 'Raznatovic', CAST('07/10/2009' as date), 1000, 'Stalno pominje gromove...'),
-		   (1006, 'Nikola', 'Simic', CAST('09/12/2009' as date), 1001, ''),
-		   (1007, 'Dragan', 'Nikolic', CAST('08/10/2010' as date), 1001, 'Buduci kralj'),
-		   (1008, 'Nikola', 'Tesla', CAST('09/28/2011' as date), 1001, 'Sokantan decko...'),
-		   (1009, 'Stevan', 'Sindjelic', CAST('03/02/2009' as date), 1001, ''),
-		   (1010, 'Sasa', 'Matic', CAST('10/26/2008' as date), 1001, 'Slabiji vid');
-	set identity_insert TAB_DECA off;
+	set identity_insert T_TIPOVI_IZVRSITELJA on;
+	insert into T_TIPOVI_IZVRSITELJA (ID, IME_TIPA, NAPLATA_USLUGE, DODATNE_INFORMACIJE)
+	values (1000, 'Bankarski', 5000.0, ''),
+		   (1001, 'Trgovinski', 6500.0, ''),
+		   (1002, 'Mobilni', 3000.0, ''),
+		   (1003, 'Maloprioritetni', 2500.0, '');
+	set identity_insert T_TIPOVI_IZVRSITELJA off;
 
-	set identity_insert TAB_FINANSIJSKE_GRUPE on;
-	insert into TAB_FINANSIJSKE_GRUPE (ID, IME_GRUPE, FINANSIJSKI_OD, FINANSIJSKI_DO, DODATNE_INFORMACIJE)
-	values (1000, 'Bez primanja', 0.0, 5000.0, ''),
-		   (1001, 'Slabija primanja', 5000.01, 15000.0, ''),
-		   (1002, 'Srednja primanja', 15000.01, 30000.0, ''),
-		   (1003, 'Jaca primanja', 30000.01, 60000.0, ''),
-		   (1004, 'Premium', 60000.01, 150000.0, ''),
-		   (1005, 'Gold', 150000.01, 500000.0, '');
-	set identity_insert TAB_FINANSIJSKE_GRUPE off;
-	
-	set identity_insert TAB_STARATELJI on;
-	insert into TAB_STARATELJI (ID, IME, PREZIME, JMBG, U_RADNOM_ODNOSU, FINANSIJSKA_GRUPA_ID, DODATNE_INFORMACIJE, STARATELJ_ID)
-	values (1000, 'Sinisa', 'Trifunovic', '123456789123', 1, 1005, '', null),
-		   (1001, 'Milos', 'Andjelkovic', '123456789012', 1, 1004, '', null),
-		   (1002, 'Nemanja', 'Tozic', '234567890123', 1, 1004, '', null),
-		   (1003, 'Andjelka', 'Trifunovic', '345678901234', 99, null, '', 1000),
-		   (1004, 'Ivana', 'Andjelkovic', '456789012345', 1, 1002, 'Dobio je kao rodjendanski poklon', 1001),
-		   (1005, 'Igor', 'Nikolic', '57890123456', 99, null, '', null),
-		   (1006, 'Svetozar', 'Sindjelic', '678901234567', 1, 1000, '', null);
-	set identity_insert TAB_STARATELJI off;
+	set identity_insert T_IZVRSITELJI on;
+	insert into T_IZVRSITELJI (ID, IME, PREZIME, SIFRA_UGOVORA, PLATA, DATUM_ZAPOSLJAVANJA, TIP_IZVRSITELJA_ID)
+	values (1000, 'Stanko', 'Bregojevic', 'a123b14c', 40000.0, getdate(), 1000),
+		   (1001, 'Mirko', 'Stojkovic', 'b234c14c', 45000.0, getdate(), 1001),
+		   (1002, 'Marija', 'Sumara', '562cd123g', 43500.0, getdate(), 1000),
+		   (1003, 'Gojko', 'Gojkovic', '34ggh123c+1', 60000.0, getdate(), 1002),
+		   (1004, 'Trifunovic', 'Sinisa', '35hgg214ff', 3000000.99, getdate(), 1003);
+	set identity_insert T_IZVRSITELJI off;
 
-	set identity_insert TAB_STARATELJI_DECA on;
-	insert into TAB_STARATELJI_DECA (ID, DETE_ID, STARATELJ_ID, ODNOS)
-	values (1000, 1001, 1000, 'Otac'),
-		   (1001, 1001, 1003, 'Majka'),
-		   (1002, 1002, 1001, 'Otac'),
-		   (1003, 1002, 1004, 'Legalna Majka'),
-		   (1004, 1003, 1002, 'Otac'),
-		   (1005, 1007, 1005, 'Otac'),
-		   (1006, 1010, 1005, 'Komsija'),
-		   (1007, 1004, 1005, 'Ujak'),
-		   (1008, 1005, 1006, 'Fan'),
-		   (1009, 1008, 1006, 'Slusalac'),
-		   (1010, 1009, 1006, 'Otac'),
-		   (1011, 1000, 1004, 'Tetka');
-	set identity_insert TAB_STARATELJI_DECA off;
+	set identity_insert T_GRUPE_ORG_JEDINICA on;
+	insert into T_GRUPE_ORG_JEDINICA (ID, IME_GRUPE, DATUM_DODAVANJA, PRIORITET, DODATNE_INFORMACIJE)
+	values (1000, 'Banka', getdate(), 1, ''),
+		   (1001, 'Mobilna', getdate(), 2, ''),
+		   (1002, 'Trgovinska', getdate(), 3, ''),
+		   (1003, 'Maloprioritetna', getdate(), 4, '');
+	set identity_insert T_GRUPE_ORG_JEDINICA off;
 
-	set identity_insert TAB_INCIDENTI on;
-	insert into TAB_INCIDENTI (ID, DETE_ID, FINANSIJSKA_ODSTETA, PRISUTNI_VASPITAC, OPIS_INCIDENTA)
-	values (1000, 1001, 30000.0, 'Veljko Domar', 'Gurnula klavir kroz prozor.'),
-		   (1001, 1008, 15000.0, 'Stasa Kuvarica', 'Zapalio osigurac');
-	set identity_insert TAB_INCIDENTI off;
+	set identity_insert T_ORGANIZACIONE_JEDINICE on;
+	insert into T_ORGANIZACIONE_JEDINICE (ID, IME_ORG_JEDINICE, KONTAKT_TELEFON, FINANSIJSKO_STANJE, DATUM_DODAVANJA,
+		GLAVNA_LOKACIJA, EMAIL_ADRESA, BROJ_ZAPOSLENIH, DODATNE_INFORMACIJE)
+	values (1000, 'Telenor', '1234-567', 20000000.0, getdate(), 'Airport City, Novi Beograd', 'telenor@telenor.rs', 1350, ''),
+		   (1001, 'Intesa', '2345-678', 50303241.0, getdate(), 'GTC Centar, Novi Beograd', 'intesa@intesa.rs', 1800, ''),
+		   (1002, 'Cvecara Bukara', '3456-123', 30000.0, getdate(), 'Neka adresa 18b', 'cvecara.bukara@gmail.com', 3, ''),
+		   (1003, 'Komercijalna', '1234-987', 50340123.0, getdate(), 'Airport City, Novi Beograd', 'komercijalna@banka.rs', 1760, '');
+	set identity_insert T_ORGANIZACIONE_JEDINICE off;
 
-	set identity_insert TAB_OSTAVLJANJA_DECE on;
-	insert into TAB_OSTAVLJANJA_DECE (ID, STARATELJ_ID, DETE_ID, VREME_OSTAVLJANJA, DODATNE_INFORMACIJE)
-	values (1000, 1001, 1002, cast('05/28/2016 07:02:31' as datetime), ''),
-		   (1001, 1006, 1008, cast('05/28/2016 07:00:00' as datetime), ''),
-		   (1002, 1005, 1010, cast('05/28/2016 07:23:00' as datetime), ''),
-		   (1003, 1006, 1005, cast('05/29/2016 07:30:23' as datetime), '');
-	set identity_insert TAB_OSTAVLJANJA_DECE off;
+	set identity_insert T_ORG_JED_GRUPE on;
+	insert into T_ORG_JED_GRUPE (ID, ORGANIZACIONA_JEDINICA_ID, GRUPA_ORG_JEDINICE_ID, INFORMACIJE_VEZE)
+	values (1000, 1000, 1001, 'telenor je mobilna mreza...'),
+		   (1001, 1000, 1000, 'telenor sadrzi telenor banku...'),
+		   (1002, 1001, 1000, 'banka intesa'),
+		   (1003, 1002, 1002, 'cvecara - trgovinska grupa'),
+		   (1004, 1003, 1000, 'komercijalna banka'),
+		   (1005, 1003, 1003, 'komercijalna banka vodi niskoprioritetne poslove (pored banke)');
+	set identity_insert T_ORG_JED_GRUPE off;
 
-	set identity_insert TAB_PREUZIMANJA_DECE on;
-	insert into TAB_PREUZIMANJA_DECE (ID, STARATELJ_ID, DETE_ID, VREME_PREUZIMANJA, DODATNE_INFORMACIJE)
-	values (1000, 1001, 1002, cast('05/28/2016 17:38:00' as datetime), ''),
-		   (1001, 1006, 1008, cast('05/28/2016 18:05:00' as datetime), ''),
-		   (1002, 1005, 1010, cast('05/28/2016 14:25:00' as datetime), ''),
-		   (1003, 1006, 1005, cast('05/29/2016 15:45:00' as datetime), '');
-	set identity_insert TAB_PREUZIMANJA_DECE off;
+	set identity_insert T_IZVRSITELJSKE_KUCE on;
+	insert into T_IZVRSITELJSKE_KUCE (ID, IME_VEZE, TRAJE_DO, DATUM_DODAVANJA, LICE_ODOBRILO, LICE_ID, 
+		ORGANIZACIONA_JEDINICA_ID, IZVRSITELJ_ID)
+	values (1000, 'veza_1', cast('01/01/2017' as date), getdate(), 99, 1000, 1000, 1001),
+		   (1001, 'veza_2', cast('01/01/2017' as date), getdate(), 1, 1000, 1001, 1002),
+		   (1002, 'veza_3', cast('01/01/2017' as date), getdate(), 1, 1000, 1003, 1003),
+		   (1003, 'veza_4', cast('01/01/2017' as date), getdate(), 99, 1001, 1000, 1004),
+		   (1004, 'veza_5', cast('01/01/2017' as date), getdate(), 1, 1001, 1003, 1002),
+		   (1005, 'veza_6', cast('01/01/2017' as date), getdate(), 1, 1002, 1000, 1000),
+		   (1006, 'veza_7', cast('01/01/2017' as date), getdate(), 99, 1002, 1001, 1000),
+		   (1007, 'veza_8', cast('01/01/2017' as date), getdate(), 1, 1002, 1002, 1003),
+		   (1008, 'veza_9', cast('01/01/2017' as date), getdate(), 1, 1002, 1003, 1004),
+		   (1009, 'veza_10', cast('01/01/2017' as date), getdate(), 1, 1003, 1003, 1004),
+		   (1010, 'veza_11', cast('01/01/2017' as date), getdate(), 1, 1004, 1001, 1002),
+		   (1011, 'veza_12', cast('01/01/2017' as date), getdate(), 99, 1004, 1002, 1000);
+	set identity_insert T_IZVRSITELJSKE_KUCE off;
 
-	set identity_insert TAB_RACUNI on;
-	insert into TAB_RACUNI (ID, UKUPNI_RACUN, STARATELJ_ID, DATUM_OTPLATE, RACUN_OTPLACEN, DODATNE_INFORMACIJE)
-	values (1000, 30000.0, 1000, getdate(), 99, ''),
-		   (1001, 35000.0, 1001, getdate(), 99, ''),
-		   (1002, 37500.0, 1002, getdate(), 99, '');
-	set identity_insert TAB_RACUNI off;
+	set identity_insert T_IZVR_KUCA_GRUPE on;
+	insert into T_IZVR_KUCA_GRUPE (ID, IZVRSITELJSKA_KUCA_ID, GRUPA_ORG_JEDINICE_ID, INFORMACIJE_VEZE)
+	values (1000, 1000, 1000, ''),
+		   (1001, 1001, 1001, ''),
+		   (1002, 1002, 1002, ''),
+		   (1003, 1003, 1003, ''),
+		   (1004, 1004, 1000, ''),
+		   (1005, 1005, 1001, ''),
+		   (1006, 1006, 1002, ''),
+		   (1007, 1007, 1003, ''),
+		   (1008, 1008, 1000, ''),
+		   (1009, 1009, 1001, ''),
+		   (1010, 1010, 1002, ''),
+		   (1011, 1011, 1003, '');
+	set identity_insert T_IZVR_KUCA_GRUPE off;
 
-	set identity_insert TAB_UPLATE on;
-	insert into TAB_UPLATE (ID, RACUN_ID, UPLATA, VREME_UPLATE, PORUKA)
-	values (1000, 1000, 15000.0, getdate(), 'prva rata'),
-		   (1001, 1000, 7000.0, getdate(), 'druga rata'),
-		   (1002, 1001, 18000.0, getdate(), 'prva rata za mesec'),
-		   (1003, 1001, 9000.0, getdate(), 'druga rata za mesec'),
-		   (1004, 1002, 37500.0, getdate(), 'cela otplata'),
-		   (1005, 1001, 8500.0, getdate(), 'poslednja rata za mesec + baksis :)');
-	set identity_insert TAB_UPLATE off;
+	set identity_insert T_DUGOVANJA on;
+	insert into T_DUGOVANJA (ID, LICE_ID, IZVRSITELJSKA_KUCA_ID, SUMA_DUGOVANJA, ROK_ISPLATE, RAZLOG)
+	values (1000, 1000, 1000, 3500.0, cast('01/01/2017' as date), ''),
+		   (1001, 1000, 1001, 4500.0, cast('02/03/2017' as date), ''),
+		   (1002, 1000, 1002, 1000.0, cast('03/05/2017' as date), ''),
+		   (1003, 1001, 1004, 500.0, cast('01/01/2017' as date), ''),
+		   (1004, 1002, 1006, 15000.0, cast('04/05/2017' as date), ''),
+		   (1005, 1002, 1008, 3700.0, cast('04/05/2017' as date), '');
+	set identity_insert T_DUGOVANJA off;
+
+	set identity_insert T_TRANSAKCIJE on;
+	insert into T_TRANSAKCIJE (ID, DUGOVANJE_ID, DATUM_UPLATE, UPLATA, PORUKA)
+	values (1000, 1000, getdate(), 3000.0, 'prva rata za isplatu'),
+		   (1001, 1000, getdate(), 200.0, 'druga rata za isplatu'),
+		   (1002, 1001, getdate(), 4500.0, ''),
+		   (1003, 1003, getdate(), 499.0, ''),
+		   (1004, 1004, getdate(), 7000.0, 'prva rata'),
+		   (1005, 1004, getdate(), 5000.0, 'druga rata');
+	set identity_insert T_TRANSAKCIJE off;
 end
